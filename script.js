aa23995a94f2b18adc37ed95e244930b140c56a4
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-16 17:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	
+	get fullname(){
+	         return console.log(this.name);
+		}
 
-class Student extends Person {}
+		set age(age){
+			this.age=age;
+		}
+}
 
-class Teacher extends Person {}
+
+
+class Student extends Person {
+	function study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	function teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
