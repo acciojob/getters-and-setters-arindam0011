a72ac442addecac105b2cf30943ebcc0ce3f779a
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-22 12:11:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//complete this code
-
 class Person {
 	constructor(name,age){
 		this._name=name;
@@ -19,13 +17,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(${this.name} is studying);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`);
+		console.log(${this.name} is teaching);
 	}
 }
 
